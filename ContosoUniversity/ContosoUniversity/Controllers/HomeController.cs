a5f408bb9d019046fe3e1b932426c999a088ed0a
2686469571 <2686469571@qq.com>
2019-01-11 13:34:33 +0800

    666

---
+++ ContosoUniversity/ContosoUniversity/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.common;
+namespace ContosoUniversity.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var data = WeatherHelper.GetWeatherByCityName("玉林");
+            return View(data);
+        }
+        //public JsonResult getweather(string city)
+        //{
+        //    var data = WeatherHelper.GetWeatherByCityName();
+        //}
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ContosoUniversity/ContosoUniversity/Models/Weather.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Weather
+    {       
+	//"reason":"successed!",
+	//"result":{
+	//	"sk":{
+	//		"temp":"11",
+	//		"wind_direction":"南风",
+	//		"wind_strength":"1级",
+	//		"humidity":"99%",
+	//		"time":"08:33"
+	//	},
+	//	"today":{
+	//		"temperature":"12℃~17℃",
+	//		"weather":"小雨",
+	//		"weather_id":{
+	//			"fa":"07",
+	//			"fb":"07"
+	//		},
+	//		"wind":"西南风微风",
+	//		"week":"星期五",
+	//		"city":"玉林",
+	//		"date_y":"2019年01月11日",
+	//		"dressing_index":"较冷",
+	//		"dressing_advice":"建议着厚外套加毛衣等服装。年老体弱者宜着大衣、呢外套加羊毛衫。",
+	//		"uv_index":"最弱",
+	//		"comfort_index":"",
+	//		"wash_index":"不宜",
+	//		"travel_index":"较不宜",
+	//		"exercise_index":"较不宜",
+	//		"drying_index":""
+	//	},
+        [DisplayName("温度")]
+        // result/today/"temperature":"12℃~17℃",
+        public string Temperature { get; set; }
+        [DisplayName("城市")]
+        //result/today/"city":"玉林",
+        public string City { get; set; }
+        [DisplayName("天气")]
+        public string WeatherInfo { get;set;}
+        [DisplayName("穿衣指数")]
+        //		"dressing_advice":"建议着厚外套加毛衣等服装。年老体弱者宜着大衣、呢外套加羊毛衫。",
+        public string Wind_strength { get; set; }
+        [DisplayName("湿度")]
+        //result/
+        public string Humidity { get; set; }
+
+        public string  Wind { get; set; }
+       
+    }
+}
